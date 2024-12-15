cceca1f772ba9c487eefde429320623295d0c798
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-12-14 21:38:37 -0800

    Added controller for user

---
+++ src/controllers/userController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { User } from "../models/user";
+
+export const getUsers = async (_req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await User.find().populate("thoughts").populate("friends");
+    res.json(users);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const createUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
